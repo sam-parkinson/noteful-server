cea268dd473b987339a07d2ac0b5d06a8c0e4de5
Sam Parkinson <parkinsonsp42@gmail.com>
2019-07-01 19:03:58 +0000

    Create service objects

--- migrations/002.do.create_noteful_notes.sql
+++ migrations/002.do.create_noteful_notes.sql
@@ -4,5 +4,5 @@ CREATE TABLE noteful_notes (
   modified TIMESTAMP DEFAULT now() NOT NULL,
   content TEXT NOT NULL,
   folder_id INTEGER
-    REFERENCES noteful_notes(id) ON DELETE CASCADE NOT NULL
+    REFERENCES noteful_folders(id) ON DELETE CASCADE NOT NULL
 );
