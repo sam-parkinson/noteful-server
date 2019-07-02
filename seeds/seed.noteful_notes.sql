f8f2fa17a325d087200f382523d8333bc0b1a9d5
Sam Parkinson <parkinsonsp42@gmail.com>
2019-07-02 20:57:36 +0000

    Add seed files

---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,9 @@
+INSERT INTO noteful_notes (note_name, folder_id, content)
+VALUES
+  ('Dogs', 1, 'This is a note about dogs.'),
+  ('Cats', 1, 'This is a note about cats.'),
+  ('More Dogs', 2, 'We have enough dogs as is right now.'),
+  ('Armadillos', 3, 'You can''t have a pet Armadillo!'),
+  ('Sasquatch', 3, 'Sasquatch is not real, so you cannot have one!'),
+  ('Fish', 2, 'The last time we had fish, they died because we forgot to feed them. So, no fish.'),
+  ('Rock', 1, 'Why do you want a pet rock? Okay, go ahead.');
